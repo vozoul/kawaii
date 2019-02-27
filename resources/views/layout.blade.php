e58ae90e333c16c462cab95a66ca227af81c4c84
ced <cedric.milius@yahoo.fr>
2019-02-27 13:13:41 +0100

    commit all work

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{asset('css/font.css')}}"/>
     <link rel="icon" type="image/png" href="{{asset('images/favicon.png')}}"/>
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 {{--<link rel="stylesheet" href="{{asset('fonts/font.css')}}"/>--}}
     @stack('style')
     <!-- Fonts -->
