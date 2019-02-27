e9e6752eb0714035d684c63ce15c2026ceaac12d
Your Name <idriss.berahal@le-campus-numerique.fr>
2019-02-27 13:24:24 +0100

    first commit by idriss

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,24 +8,27 @@ class PagesController extends Controller
 {
     public function viewHomePage()
     {
-     return view ('home');
+        return view('homepage');
     }
-    public function viewListProductPage()
+    public function viewProductPage()
     {
-    return view ('listproducts');
+        return view('productpage');
     }
-    public function viewContactPage()
+    public function viewProductsList()
     {
-        return view ('contact');
-    }
-    public function viewListBasket()
-    {
-        return view ('basket');
-    }
-    public function viewProduct()
-    {
-        return view ('product');
-    }
+        $arrayProduct = array
+        (
+            array ( "image"=> asset("images/61S1hqNrZoL._UX679_.jpg"), "name"=> 'Chaussettes longue', "price"=> 32),
+            array ( "image"=> asset("images/61VKfLvAJ1L._UX679_.jpg"), "name"=> 'Robe butterfly', "price"=> 50),
+            array ( "image"=> asset("images/téléchargement (3).jpg"), "name"=> 'Mitaines chat', "price"=> 12),
+            array ( "image"=> asset("images/téléchargement.jpg"), "name"=> 'T-shirt sushi', "price"=> 24)
+        );
 
+        return view('productslist',["arrayProduct"=>$arrayProduct]);
 
+    }
+    public function viewBasket()
+    {
+        return view('basket');
+    }
 }
