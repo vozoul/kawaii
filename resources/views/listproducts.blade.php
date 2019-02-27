2aad0389c52a4c81673cc8d1e5486e2cfd0541a5
Your Name <idriss.berahal@le-campus-numerique.fr>
2019-02-27 13:24:24 +0100

    first commit by idriss

--- resources/views/listproducts.blade.php
+++ resources/views/listproducts.blade.php
@@ -1,8 +1,29 @@
 @extends('layout')
-@section('title', 'listproducts')
-@push('style')
-    <link rel="stylesheet" href="{{asset('css/produits.css')}}"/>
-@endpush
+@section('title','productslist')
+<link rel="stylesheet" href="{{asset('css/responsive.css')}}" />
+
+
 @section('content')
-    <h1>liste des produits</h1>
+
+
+<div class="container">
+    <div class="row justify-content-center border-left border-right">
+        @foreach($arrayProduct as $product)
+    <div class="  card  align my-3 mx-2" style="width: 18rem ;background-color: #FFD899 ">
+        <div class="  pe-7s-like  ">+</div>
+        <div class="  pe-7s-shopbag ">+</div>
+
+        <img src="{{$product['image']}}" class=" card-img-top border-bottom bor " alt="photo">
+        <div class="card-body">
+            <p class="card-text text-uppercase text-center">{{$product['name']}}</p>
+            <p class="card-text text-uppercase text-center">{{$product['price']}} &#128;</p>
+
+
+        </div>
+
+    </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
