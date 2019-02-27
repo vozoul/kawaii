e4294bdeed7d697ef5b01053e615b67380cf8b19
vincent <vozoul@gmail.com>
2019-02-27 13:50:17 +0100

    vince work

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,3 @@ Route::get('/contact', 'PagesController@viewContactPage');
 Route::get('/listproducts', 'PagesController@viewListProductPage');
 Route::get('/product', 'PagesController@viewProduct');
 Route::get('/basket', 'PagesController@viewListBasket');
-<<<<<<< HEAD
-=======
-Route::get('/register','PagesController@viewRegister');
->>>>>>> e661d7d74184c6a0a1053ff70bf2a5e8836111af
-
-
-
-
-
