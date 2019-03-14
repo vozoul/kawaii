7302d2de31e19d6d32ad937e1f7aa96920657c63
berrahal-idriss <idriss.berahal@le-campus-numerique.fr>
2019-03-14 16:31:29 +0100

    autentification en cours

---
+++ app/Http/categoriesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 13/03/2019
+ * Time: 10:16
+ */
+
+namespace App\Http;
+
+
+class categoriesController
+{
+
+}
---
+++ resources/views/underwear.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+@section('title', 'clothing')
+@push('style')
+<link rel="stylesheet" href="{{asset('css/cherii.css')}}"/>
+@endpush
+@section('content')
+
+
+
+
+
+<div class="container">
+    <div class="row justify-content-center border-left border-right">
+        @foreach($products as $product)
+
+
+
+        <div class="  card  align my-3 mx-2" style="width: 18rem ;background-color: #FFD899 ">
+            <div class="  pe-7s-like  ">+</div>
+            <div class="  pe-7s-shopbag ">+</div>
+            <img src="{{$product->picture}}" class=" card-img-top border-bottom bor " alt="photo">
+            <div class="card-body">
+                <p class="card-text text-uppercase text-center">{{$product->name}}</p>
+                <p class="card-text text-uppercase text-center">{{$product->price}} &#128;</p>
+                <p class="card-text text-uppercase text-center">{{$product->description}}</p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+
+@endsection
+</div>
